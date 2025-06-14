22f25fcfb8fda49fadf4b1726bf29bc471d5bca4
maertin <mutuverria00gmail.com>
2025-06-14 16:11:08 -0300

    agrega endpoints a las rutas

--- backend/src/controller/usuario.controller.ts
+++ backend/src/controller/usuario.controller.ts
@@ -5,9 +5,12 @@ import UsuarioDTO from "../dtos/usuario.dto";
 export class UsuarioController {
   constructor(private usuarioService: UsuarioService) {}
 
-  public crearUsuario = async (req: Request, res: Response) => {
+  public crearUsuario = async (req: Request, res: Response): Promise<void> => {
     try {
       const { nombre, apellido, email, contraseña, direccion } = req.body;
+
+      if(!nombre || !apellido || !email || !contraseña || !direccion) res.status(400).json({ error: "Todos los campos son obligatorios" });
+
       const usuario: UsuarioDTO = {
         nombre,
         apellido,
@@ -24,7 +27,10 @@ export class UsuarioController {
     }
   };
 
-  public obtenerUsuarios = async (req: Request, res: Response) => {
+  public obtenerUsuarios = async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
     try {
       const users = await this.usuarioService.obtenerUsuarios();
       res.status(200).json(users);
@@ -34,40 +40,55 @@ export class UsuarioController {
     }
   };
 
-  public obtenerUsuarioPorEmail = async(req: Request, res: Response) => {
+  public obtenerUsuarioPorEmail = async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
     try {
-      const { email } = req.params;
+      const { email } = req.body;
+      if(!email) res.status(400).json({ error: "Email es obligatorio" });
+
       const user = await this.usuarioService.obtenerUsuarioPorEmail(email);
-      if(!user) return res.status(404).json({ error: "Usuario no encontrado" });
-      res.status(200).json(user);
+
+      if (!user) res.status(404).json({ error: "Usuario no encontrado" });
+      else res.status(200).json(user);
     } catch (error) {
       console.error("Error fetching user:", error);
       res.status(500).json({ error: "Internal server error" });
     }
-  }
+  };
 
-  public iniciarSesion = async(req: Request, res: Response) =>{
-    try{
-      const { email, contraseña} = req.body;
-      if(!email || !contraseña) return res.status(400).json({ error: "Email y contraseña son obligatorios" });
+  public iniciarSesion = async (req: Request, res: Response) => {
+    try {
+      const { email, contraseña } = req.body;
+      if (!email || !contraseña)
+        res.status(400).json({ error: "Email y contraseña son obligatorios" });
+
       const user = await this.usuarioService.iniciarSesion(email, contraseña);
-      if(!user) return res.status(401).json({ error: "Credenciales incorrectas" });
-      res.status(200).json(user);
-    }catch(error){
+
+      if (!user) res.status(401).json({ error: "Credenciales incorrectas" });
+      else res.status(200).json(user);
+    } catch (error) {
       console.error("Error al iniciar sesión:", error);
       res.status(500).json({ error: "Internal server error" });
     }
-  }
+  };
 
-  public actualizarContraseña = async(req: Request, res: Response) => {
+  public actualizarContraseña = async (req: Request, res: Response) => {
     try {
       const { email, nuevaContraseña } = req.body;
-      const user = await this.usuarioService.actualizarContraseña(email, nuevaContraseña);
-      if(!user) return res.status(404).json({ error: "Usuario no encontrado" });
-      res.status(200).json(user);
+      if (!email || !nuevaContraseña)
+        res.status(400).json({ error: "Email y contraseña son obligatorios" });
+
+      const user = await this.usuarioService.actualizarContraseña(
+        email,
+        nuevaContraseña
+      );
+      if (!user) res.status(404).json({ error: "Usuario no encontrado" });
+      else res.status(200).json(user);
     } catch (error) {
       console.error("Error al actualizar la contraseña:", error);
       res.status(500).json({ error: "Internal server error" });
     }
-  }
+  };
 }

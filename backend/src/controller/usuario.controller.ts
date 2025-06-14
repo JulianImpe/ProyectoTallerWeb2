9bf0046fc9dfb883a05a4f35080306cbe8db2dca
maertin <mutuverria00gmail.com>
2025-06-14 15:36:24 -0300

    agrega encriptacion de contraseña

--- backend/src/controller/usuario.controller.ts
+++ backend/src/controller/usuario.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { UsuarioService } from "./../services/usuario.service";
-import { Usuario } from "../../generated/prisma";
 import UsuarioDTO from "../dtos/usuario.dto";
 
 export class UsuarioController {
@@ -34,4 +33,41 @@ export class UsuarioController {
       res.status(500).json({ error: "Internal server error" });
     }
   };
+
+  public obtenerUsuarioPorEmail = async(req: Request, res: Response) => {
+    try {
+      const { email } = req.params;
+      const user = await this.usuarioService.obtenerUsuarioPorEmail(email);
+      if(!user) return res.status(404).json({ error: "Usuario no encontrado" });
+      res.status(200).json(user);
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+
+  public iniciarSesion = async(req: Request, res: Response) =>{
+    try{
+      const { email, contraseña} = req.body;
+      if(!email || !contraseña) return res.status(400).json({ error: "Email y contraseña son obligatorios" });
+      const user = await this.usuarioService.iniciarSesion(email, contraseña);
+      if(!user) return res.status(401).json({ error: "Credenciales incorrectas" });
+      res.status(200).json(user);
+    }catch(error){
+      console.error("Error al iniciar sesión:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+
+  public actualizarContraseña = async(req: Request, res: Response) => {
+    try {
+      const { email, nuevaContraseña } = req.body;
+      const user = await this.usuarioService.actualizarContraseña(email, nuevaContraseña);
+      if(!user) return res.status(404).json({ error: "Usuario no encontrado" });
+      res.status(200).json(user);
+    } catch (error) {
+      console.error("Error al actualizar la contraseña:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
 }
--- backend/src/services/usuario.service.ts
+++ backend/src/services/usuario.service.ts
@@ -1,21 +1,23 @@
 import { PrismaClient, Usuario } from "../../generated/prisma";
+import { cifrarContraseña, compararContraseñas } from "../auth/bcrypt";
 import UsuarioDTO from "../dtos/usuario.dto";
 export class UsuarioService {
   private prisma = new PrismaClient();
   public async crearUsuario(usuario: UsuarioDTO): Promise<Usuario> {
     try {
+      const contraseñaCifrada = await cifrarContraseña(usuario.contraseña);
       const newUser = await this.prisma.usuario.create({
         data: {
           nombre: usuario.nombre,
           apellido: usuario.apellido,
           email: usuario.email,
-          contrasena: usuario.contraseña,
+          contrasena: contraseñaCifrada,
           direccion: usuario.direccion,
         },
       });
       return newUser;
     } catch (error) {
-      console.error("Error creating user:", error);
+      console.error("Error al crear usuario:", error);
       throw new Error("Internal server error");
     }
   }
@@ -25,7 +27,49 @@ export class UsuarioService {
       const users = await this.prisma.usuario.findMany({});
       return users;
     } catch (error) {
-      console.error("Error fetching users:", error);
+      console.error("Error al obtener los usuarios:", error);
+      throw new Error("Internal server error");
+    }
+  }
+
+  public async obtenerUsuarioPorEmail(email: string): Promise<Usuario | null> {
+    try {
+      const user = await this.prisma.usuario.findUnique({
+        where: { email },
+      });
+      return user;
+    } catch (error) {
+      console.error("Error al obtener el usuario por email:", error);
+      throw new Error("Internal server error");
+    }
+  }
+
+  public async actualizarContraseña(email: string, nuevaContraseña: string) {
+    try {
+      const contraseñaCifrada = await cifrarContraseña(nuevaContraseña);
+      const user = await this.prisma.usuario.update({
+        where: { email },
+        data: {
+          contrasena: contraseñaCifrada,
+        },
+      });
+      return user;
+    } catch (error) {
+      console.error("Error al actualizar la contraseña:", error);
+      throw new Error("Internal server error");
+    }
+  }
+
+  public async iniciarSesion(email: string, contraseña: string) {
+    try {
+      const user = await this.prisma.usuario.findUnique({
+        where: { email },
+      });
+      if (user && (await compararContraseñas(contraseña, user.contrasena)))
+        return user;
+      else return null;
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error);
       throw new Error("Internal server error");
     }
   }

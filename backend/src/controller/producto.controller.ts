3a9fccbbb08e89462325e8f514a6f6bf46307912
maertin <mutuverria00gmail.com>
2025-06-14 15:34:50 -0300

    agrega endpoints para usuario

--- backend/src/controller/producto.controller.ts
+++ backend/src/controller/producto.controller.ts
@@ -2,35 +2,51 @@ import { ProductoDTO } from "../dtos/producto.dto";
 import { ProductoService } from "../services/producto.service";
 import { Request, Response } from "express";
 
-export class ProductoController{
-    constructor(private productoService: ProductoService){}
+export class ProductoController {
+  constructor(private productoService: ProductoService) {}
 
-    public obtenerProductos = async (req: Request, res: Response) => {
-        try {
-          const products = await this.productoService.obtenerProductos();
-          res.status(200).json(products);
-        } catch (error) {
-          console.error("Error fetching products:", error);
-          res.status(500).json({ error: "Internal server error" });
-        }
+  public obtenerProductos = async (req: Request, res: Response) => {
+    try {
+      const products = await this.productoService.obtenerProductos();
+      res.status(200).json(products);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
+
+  public crearProducto = async (req: Request, res: Response) => {
+    try {
+      const { nombre, descripcion, precio, stock, imagen, clasificacion } =
+        req.body;
+      const producto: ProductoDTO = {
+        nombre,
+        descripcion,
+        precio,
+        stock,
+        imagen,
+        clasificacion,
       };
-    public crearProducto = async (req: Request, res: Response) => {
-        try {
-          const { nombre, descripcion, precio, stock, imagen, clasificacion } = req.body;
-          const producto: ProductoDTO = {
-            nombre,
-            descripcion,
-            precio,
-            stock,
-            imagen,
-            clasificacion,
-          };
-    
-          const newProduct = await this.productoService.crearProducto(producto);
-          res.status(201).json(newProduct);
-        } catch (error) {
-          console.error("Error creating product:", error);
-          res.status(500).json({ error: "Internal server error" });
-        }
-      };
+
+      const newProduct = await this.productoService.crearProducto(producto);
+      res.status(201).json(newProduct);
+    } catch (error) {
+      console.error("Error creating product:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
+
+  public obtenerProductoPorId = async (req: Request, res: Response) => {
+    try {
+      const id = parseInt(req.params.id);
+      const producto = await this.productoService.obtenerProductoPorId(id);
+      if (!producto) {
+        return res.status(404).json({ error: "Producto no encontrado" });
+      }
+      res.status(200).json(producto);
+    } catch (error) {
+      console.error("Error al obtener el producto:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
 }
--- backend/src/services/producto.service.ts
+++ backend/src/services/producto.service.ts
@@ -14,24 +14,34 @@ export class ProductoService {
     }
   }
 
-    public async crearProducto(producto: ProductoDTO): Promise<Producto> {
-        try {
-        const newProduct = await this.prisma.producto.create({
-            data: {
-            nombre: producto.nombre,
-            descripcion: producto.descripcion,
-            precio: producto.precio,
-            stock: producto.stock,
-            imagen: producto.imagen,
-            clasificacion: producto.clasificacion,
-            },
-        });
-        return newProduct;
-        } catch (error) {
-        console.error("Error creating product:", error);
-        throw new Error("Internal server error");
-        }
+  public async crearProducto(producto: ProductoDTO): Promise<Producto> {
+    try {
+      const newProduct = await this.prisma.producto.create({
+        data: {
+          nombre: producto.nombre,
+          descripcion: producto.descripcion,
+          precio: producto.precio,
+          stock: producto.stock,
+          imagen: producto.imagen,
+          clasificacion: producto.clasificacion,
+        },
+      });
+      return newProduct;
+    } catch (error) {
+      console.error("Error creating product:", error);
+      throw new Error("Internal server error");
     }
+  }
 
-    
+  public async obtenerProductoPorId(id: number): Promise<Producto | null> {
+    try {
+      const product = await this.prisma.producto.findUnique({
+        where: { id },
+      });
+      return product;
+    } catch (error) {
+      console.error("Error al obtener el producto por ID:", error);
+      throw new Error("Internal server error");
+    }
+  }
 }

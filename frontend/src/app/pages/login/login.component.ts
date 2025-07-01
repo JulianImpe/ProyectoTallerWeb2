c82ff119fba464cd5f5e14d4a9e6edce3cba9e4e
maertin <mutuverria00gmail.com>
2025-06-30 22:08:46 -0300

    modifica formulario de login

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { CredencialesLogin, UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor() {}
 
+  form!: FormGroup;
+
+  private fb = inject(FormBuilder);
+
+  messageService = inject(MessageService);
+  usuarioService = inject(UserService);
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', Validators.required, Validators.email],
+      contraseña: ['', Validators.required],
+    });
+  }
+
+  iniciarSesion() {
+    if (!this.form.valid) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Formulario no válido',
+      });
+      return;
+    }
+
+    const credenciales: CredencialesLogin = {
+      email: this.form.value.username,
+      contraseña: this.form.value.password,
+    };
+
+    this.usuarioService.iniciarSesion(credenciales).subscribe({
+      next: (response) => {
+        console.log('Inicio de sesión exitoso', response);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Bienvenido',
+          detail: 'Sesión iniciada correctamente',
+        });
+      },
+      error: (error) => {
+        console.error('Error al iniciar sesión:', error);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Credenciales incorrectas',
+        });
+      },
+    });
+  }
 }
